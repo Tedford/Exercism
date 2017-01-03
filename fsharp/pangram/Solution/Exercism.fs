31545fcca3e9f05bafd8c7680021d1bd9c2c3a3f
Tedford <dev@tedfordjohnson.com>
2017-01-03 10:52:29 -0500

    pangram
    solution to the pangram F# exercise

--- fsharp/pangram/Solution/Exercism.fs
+++ fsharp/pangram/Solution/Exercism.fs
@@ -1,3 +1,10 @@
 ﻿module Pangram
 
-let isPangram message = true
+let isLetter c = System.Text.RegularExpressions.Regex.IsMatch(c.ToString(),"(?i)[a-z]")
+
+let isPangram (message:string) = 
+    let contents =
+        message.ToCharArray()
+        |> Seq.filter isLetter
+        |> Seq.groupBy (fun c -> c)
+    contents |> Seq.length > 25
