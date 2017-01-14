9db0dbfc9a6464f3c41e13974a4c7951dbc61997
Tedford <dev@tedfordjohnson.com>
2017-01-14 09:24:24 -0500

    [F#] Triangle
    initial checkin and solution

---
+++ fsharp/triangle/Solution/Exercism.fs
@@ -0,0 +1,12 @@
+﻿module Triangle
+
+
+type TriangleKind = Equilateral | Isosceles | Scalene
+
+let kind s1 s2 s3 =
+    match (s1,s2,s3) with
+    | _ when s1 <= 0m || s2 <= 0m|| s3 <= 0m-> invalidOp "The length of a side must be greater than 0"
+    | _ when s1 + s2 < s3 || s1 + s3 < s2 || s2 + s3 < s1 -> invalidOp "Triangle inequality theorem violated"
+    | _ when s1 = s2 && s1=s3-> TriangleKind.Equilateral
+    | _ when s1 = s2 || s2 = s3 || s1 = s3 -> TriangleKind.Isosceles
+    | _ -> TriangleKind.Scalene
