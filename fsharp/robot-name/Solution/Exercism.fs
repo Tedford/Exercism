e820867b4bded5d4f1f97b52a02310c10ffa4e5b
Tedford <dev@tedfordjohnson.com>
2017-01-16 00:29:51 -0500

    [F#] Robot Name
    Solution

--- fsharp/robot-name/Solution/Exercism.fs
+++ fsharp/robot-name/Solution/Exercism.fs
@@ -1,2 +1,21 @@
 ﻿module RobotName
+open System
 
+let Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let Digits = "0123456789"
+let rand = Random()
+let generate = fun () -> rand.Next()
+
+let generateName seed= 
+    seq {for i in 1..5 -> 
+            let index = generate()
+            let source = if i < 3 then Alphabet else Digits
+            source.[index % source.Length]
+    }
+    |> String.Concat
+
+
+type Robot = {Name:string}
+let mkRobot value= {Name = generateName value}
+let reset (robot:Robot) = {Name = generateName robot}
+let name (robot:Robot) = robot.Name
