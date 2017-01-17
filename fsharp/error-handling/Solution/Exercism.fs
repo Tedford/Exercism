07e46da564a599f98a2a94c03475661740d7a4f1
Tedford <dev@tedfordjohnson.com>
2017-01-17 09:25:01 -0500

    [F#] Error Handing
    Problem and Solution

---
+++ fsharp/error-handling/Solution/Exercism.fs
@@ -0,0 +1,32 @@
+﻿module ErrorHandling
+
+type Result<'TSuccess, 'TError> = 
+    | Ok of 'TSuccess
+    | Error of 'TError
+
+let (|Int|_|) str = 
+    match System.Int32.TryParse(str) with
+    | (true, int) -> Some(int)
+    |  _ -> None
+
+let handleErrorByThrowingException value = failwith "Danger, Will Robinson, Danger"
+
+let handleErrorByReturningOption value = 
+    match value with
+    | Int i  -> Some(i)
+    | _ -> None
+     
+let handleErrorByReturningResult value = 
+    match value with
+    | Int i -> Ok(i)
+    | _ -> Error("Could not convert input to integer")
+
+let cleanupDisposablesWhenThrowingException resource = 
+    use r = resource
+    failwith "Danger, Will Robinson, Danger"
+    
+let bind next = 
+    fun input ->
+    match input with
+    | Ok value -> next value
+    | Error value -> Error value
