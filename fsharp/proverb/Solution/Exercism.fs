47e63829203778dde008f478ab4266f6f63436a6
Tedford <dev@tedfordjohnson.com>
2017-01-18 10:32:39 -0500

    [F#] Proverb
    Problem statement and Solution

---
+++ fsharp/proverb/Solution/Exercism.fs
@@ -0,0 +1,8 @@
+﻿module Proverb
+
+let poem = [|"For want of a nail the shoe was lost."; "For want of a shoe the horse was lost."; "For want of a horse the rider was lost."; "For want of a rider the message was lost."; "For want of a message the battle was lost."; "For want of a battle the kingdom was lost."; "And all for the want of a horseshoe nail."|]
+
+let line index = poem.[index-1]
+
+let proverb = System.String.Join("\n",poem)
+    
