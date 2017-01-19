8bf3df8710133657e0790a94d11b6c3ff8107f6f
Tedford <dev@tedfordjohnson.com>
2017-01-19 09:00:28 -0500

    [F#] Protein Translation
    Problem and Solution

---
+++ fsharp/protein-translation/Solution/Exercism.fs
@@ -0,0 +1,29 @@
+﻿module ProteinTranslation
+
+let proteins = [("AUG","Methionine");
+                ("UUU", "Phenylalanine");
+                ("UUC", "Phenylalanine");
+                ("UUA", "Leucine");
+                ("UUG", "Leucine");
+                ("UCU", "Serine");
+                ("UCC", "Serine");
+                ("UCA", "Serine");
+                ("UCG", "Serine");
+                ("UAU", "Tyrosine");
+                ("UAC", "Tyrosine");
+                ("UGU", "Cysteine");
+                ("UGC", "Cysteine");
+                ("UGG", "Tryptophan");
+                ] |> Map.ofList
+
+let translate polypeptide = 
+    List.unfold (fun sequence -> 
+        if System.String.IsNullOrWhiteSpace(sequence) 
+        then None 
+        else 
+            let codon =sequence.Substring(0,3)
+            match codon with
+            | "UAA" | "UAG" | "UGA" -> None
+            | _-> Some(codon, sequence.Substring(3)) 
+        ) polypeptide
+    |> List.map (fun codon -> proteins.[codon])
