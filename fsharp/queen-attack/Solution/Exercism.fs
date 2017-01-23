82c696331e0e1cbcd56c3c48e19037fe9f7d874d
Tedford <dev@tedfordjohnson.com>
2017-01-22 22:02:05 -0500

    [F#] Queen Attack
    Problem and Solution

---
+++ fsharp/queen-attack/Solution/Exercism.fs
@@ -0,0 +1,8 @@
+﻿module QueenAttack
+
+let canAttack white black = 
+    if white = black then failwith (sprintf "Cannot occuply the same square %A" white)
+    match white, black with 
+    | (x1,y1), (x2, y2) when x1 = x2 || y1 = y2  -> true
+    | (x1,y1), (x2, y2) when abs(x2 - x1) = abs(y2-y1) -> true
+    | _ -> false
