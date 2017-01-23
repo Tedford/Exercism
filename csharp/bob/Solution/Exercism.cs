4f9e0d0b7fd57154ba50db5f5d5cbdf12ea15074
Tedford <dev@tedfordjohnson.com>
2017-01-22 22:31:20 -0500

    [C#] Bob
    Problem and Solution

---
+++ csharp/bob/Solution/Exercism.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+public static class Bob
+{
+    
+    private static Regex shouting = new Regex("^[\\d,\\s]+[A-Z]+!$|^(?:[A-Z]+[^a-z]+)[!.?]?$", RegexOptions.Compiled);
+
+    public static string Hey(string message)
+    {
+        message = message.Trim();
+        string response = string.Empty;
+        if( string.IsNullOrWhiteSpace(message))
+        {
+            response = "Fine. Be that way!";
+        }
+        else if (shouting.IsMatch(message))
+        {
+            response = "Whoa, chill out!";
+        }
+        else if(message.EndsWith("?"))
+        {
+            response = "Sure.";
+        }
+        else
+        {
+            response = "Whatever.";
+        }
+        return response;
+    }
+}
