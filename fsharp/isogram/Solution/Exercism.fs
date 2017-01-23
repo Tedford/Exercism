6f0e5eedac68f0fbd01ba31325da01a6e5b35149
Tedford <dev@tedfordjohnson.com>
2017-01-23 18:53:29 -0500

    [F#] Isogram
    Problem and Solution

---
+++ fsharp/isogram/Solution/Exercism.fs
@@ -0,0 +1,9 @@
+﻿module Isogram
+
+let isogram (word:string) =
+    word.ToLower().ToCharArray()
+    |> Seq.groupBy id
+    |> Seq.map(fun (id, occurance)-> (id, Seq.length occurance))
+    |> Seq.filter (fun(id, count) -> System.Char.IsLetter(id))
+    |> Seq.filter (fun (id,count)-> count > 1)
+    |> Seq.length = 0
