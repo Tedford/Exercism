b5745079cdb3c39789f53b074c183a7a2ec06e49
Tedford <dev@tedfordjohnson.com>
2017-01-29 12:39:18 -0500

    [F#] Perfect Numbers
    Problem and Solution

---
+++ fsharp/perfect-numbers/Solution/Exercism.fs
@@ -0,0 +1,18 @@
+﻿module PerfectNumbers
+
+type NumberType = Perfect | Deficient | Abundant
+
+let classifySum number sum=
+    match sum,number with
+    | _ when sum = number-> Perfect
+    | _ when sum > number-> Abundant
+    | _ -> Deficient
+
+let getDivisors number = 
+    [1..number - 1]
+    |> List.filter ( fun i-> number % i = 0 )
+
+let classify number = 
+    getDivisors number
+    |> List.sum
+    |> classifySum number
