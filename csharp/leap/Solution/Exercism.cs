36d6200ddbcc84a572f22b0810c3b2aaeb31bcc6
Tedford <dev@tedfordjohnson.com>
2017-01-29 12:37:13 -0500

    [C#] Leap
    Problem and Solution

---
+++ csharp/leap/Solution/Exercism.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class Year
+{
+    public static bool IsLeap(int year)
+    {
+        Func<int, int, bool> isDivisble = (value, multiple) => value % multiple == 0;
+        bool by4 = isDivisble(year, 4);
+        bool by100 = isDivisble(year, 100);
+        bool by400 = isDivisble(year, 400);
+
+        return by4 && (!by100 || by100 && by400);
+    }
+}
