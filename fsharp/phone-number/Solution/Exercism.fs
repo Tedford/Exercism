e93ad2f9764d5e7661719ac383873ce75410db6d
Tedford <dev@tedfordjohnson.com>
2017-01-29 12:39:45 -0500

    [F#] PhoneNumber
    Problem and Solution

---
+++ fsharp/phone-number/Solution/Exercism.fs
@@ -0,0 +1,15 @@
+﻿module PhoneNumber
+
+let IsNumber value =
+    let isDefined =not( System.String.IsNullOrWhiteSpace(value))  
+    let firstDigit = if isDefined then value.[0] else 'z'
+    match isDefined, value.Length, firstDigit with
+    | true, 10, _-> Some(value)
+    | true, 11, '1' -> Some(value.Substring(1))
+    | _ -> None
+
+let parsePhoneNumber input =
+    input 
+    |> Seq.filter (fun c -> System.Char.IsDigit(c))
+    |> System.String.Concat
+    |> IsNumber
