f5545c05f04280205eaf9dc7367676d9f74708ab
Tedford <dev@tedfordjohnson.com>
2017-02-05 16:25:51 -0500

    [F#] Anagram
    Solution and Problem

---
+++ fsharp/anagram/Solution/Exercism.fs
@@ -0,0 +1,16 @@
+﻿module Anagram
+
+let reduce (word:string) =
+    word.ToLower() 
+    |> Seq.groupBy id
+    |> Seq.map (fun (letter, occurances)-> (letter, Seq.length occurances))
+    |> Map.ofSeq
+
+let anagrams (candidates:string list) (word:string) = 
+    let target = reduce word
+
+    candidates
+    |> Seq.map (fun w -> (w, reduce w))
+    |> Seq.filter (fun (w, token) -> token = target && (System.String.Equals(w, word, System.StringComparison.InvariantCultureIgnoreCase) = false))
+    |> Seq.map (fun (word,_)-> word)
+    
