7dce980698138782f5fbae957860fd2f846cf332
Tedford <dev@tedfordjohnson.com>
2017-02-05 00:19:47 -0500

    [F#] Allergies
    Problem and Solution

---
+++ fsharp/allergies/Solution/Exercism.fs
@@ -0,0 +1,23 @@
+﻿module Allergies
+
+type  Allergen =
+| Eggs = 1
+| Peanuts = 2
+| Shellfish = 4
+| Strawberries = 8
+| Tomatoes = 16
+| Chocolate = 32  
+| Pollen = 64
+| Cats = 128
+
+
+let allergies score = 
+    System.Enum.GetValues(typeof<Allergen>)
+    |> Seq.cast<Allergen>
+    |> Seq.map (fun allergen -> allergen,  (int allergen) &&& score = (int allergen))
+    |> Seq.filter (fun (_, active) -> active)
+    |> Seq.map (fun (allergen, active) -> allergen)
+    |> Seq.toList
+
+
+let allergicTo (allergen:Allergen) score = (int allergen) &&& score = (int allergen)
