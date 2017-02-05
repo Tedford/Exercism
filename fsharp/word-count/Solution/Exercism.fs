e7b66c2638bce71ec97dca2a5629a102186c7f87
Tedford <dev@tedfordjohnson.com>
2017-02-04 21:18:30 -0500

    [F#] Word Count
    Problem and Solution

---
+++ fsharp/word-count/Solution/Exercism.fs
@@ -0,0 +1,9 @@
+﻿module Phrase
+open System.Text.RegularExpressions
+
+let wordCount (phrase:string) =
+
+    Regex.Matches(phrase.ToLower(),@"\b[\w']+\b")
+    |> Seq.cast<Match>
+    |> Seq.countBy (fun m-> m.Value)
+    |> Map.ofSeq    
