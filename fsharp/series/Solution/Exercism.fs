03026d2bad960a098f81b68055b4a7819e3e30b5
Tedford <dev@tedfordjohnson.com>
2017-02-05 23:47:09 -0500

    [F#] Series
    Problem and Solution

---
+++ fsharp/series/Solution/Exercism.fs
@@ -0,0 +1,7 @@
+﻿module Series
+
+let slices (data:string) length = 
+    let digits = data.ToCharArray() |> Array.toList |>  List.map (fun c->System.Int32.Parse(c.ToString()))
+    match length > data.Length with
+    | true -> failwith "The window must be larger than the data stream"
+    | false ->  [0 .. Seq.length digits - length] |> List.map (fun index -> digits |> List.skip index |> List.take length)
