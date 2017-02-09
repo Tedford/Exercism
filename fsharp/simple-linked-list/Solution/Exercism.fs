d8e65ccac37e9715050a2f4d2e3ea7b216b7e832
Tedford <dev@tedfordjohnson.com>
2017-02-08 20:45:43 -0500

    [F#] Simple Linked Lists
    Problem and partial solution

---
+++ fsharp/simple-linked-list/Solution/Exercism.fs
@@ -0,0 +1,20 @@
+﻿module SimpleLinkedList
+
+
+type Node = 
+| End of int
+| Element of Node * int
+
+let nil = None
+
+let create value next = Node.End
+
+
+let isNil node = false
+
+let next (node:Node) = node
+
+let datum node = 
+    match node with
+    | Element(n,v) -> Some(v)
+    | End(v) -> Some(v)
