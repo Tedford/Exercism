eeb7a1175419235aced058e1cf64849e5c882a4e
Tedford <dev@tedfordjohnson.com>
2017-02-12 17:54:41 -0500

    [F#] Simple Linked List
    Solution

--- fsharp/simple-linked-list/Solution/Exercism.fs
+++ fsharp/simple-linked-list/Solution/Exercism.fs
@@ -2,19 +2,41 @@
 
 
 type Node = 
-| End of int
 | Element of Node * int
+| Empty
 
-let nil = None
+let nil = Empty
 
-let create value next = Node.End
+let isNil node = 
+    match node with
+    | Empty -> true
+    | _ -> false
 
+let create value next = Element(next, value)
 
-let isNil node = false
-
-let next (node:Node) = node
+let next node = 
+    match node with
+    | Empty -> failwith "The list is empty"
+    | Element(n,_) -> n
 
 let datum node = 
     match node with
-    | Element(n,v) -> Some(v)
-    | End(v) -> Some(v)
+    | Element(_,v) -> v
+    | Empty -> failwith "The list is empty"
+
+let toList linkedList = 
+    let rec extract node =
+        match node with
+        | Empty -> []
+        | Element(n,v)-> v :: extract n
+    extract linkedList
+
+let fromList l = 
+    let rec makeNode list =
+        match list with
+        | [] -> Empty
+        | head :: tail -> Element(makeNode tail, head)
+    makeNode l
+
+let reverse l = l |> toList |> List.rev |> fromList
+    
