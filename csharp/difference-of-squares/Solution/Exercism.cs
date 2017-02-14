bbe883182516e8f16bb1c8d7347848f8fb556ce7
Tedford <dev@tedfordjohnson.com>
2017-02-14 09:03:27 -0500

    [C#] Difference of Squares
    Problem and Solution

---
+++ csharp/difference-of-squares/Solution/Exercism.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+public class Squares
+{
+    public int Value { get; }
+
+    public Squares(int value)
+    {
+        if (value < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(value),"A value of 0 or greater must be supplied.");
+        }
+        Value = value;
+    }
+
+    public int SquareOfSums() =>(int)Math.Pow(Enumerable.Range(1, Value).Sum(), 2);
+            
+
+    public int SumOfSquares() => Enumerable.Range(1,Value).Select(i=> i*i).Sum();
+
+    public int DifferenceOfSquares() => SquareOfSums() - SumOfSquares();
+}
