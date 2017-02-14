2b751d96485dddb5e7c1a63b4d1b1d9c5d4c5b7f
Tedford <dev@tedfordjohnson.com>
2017-02-14 12:12:23 -0500

    [C#] Sum of Multiples
    Problem and Solution

---
+++ csharp/sum-of-multiples/Solution/Exercism.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    private static bool IsMultiple(int candidate, int number) => candidate % number == 0;
+
+    public static int To(IEnumerable<int> factors, int value)
+    {
+        return Enumerable.Range(1, value-1)
+        .Where(i => factors.Any(f => IsMultiple(i, f)))
+        .Sum();
+    }
+}
+
