084a0900e6576a0ba251a94fd62edd875a33a098
Tedford <dev@tedfordjohnson.com>
2017-02-14 09:23:28 -0500

    [C#] RNA Transcription
    Problem and Solution

---
+++ csharp/rna-transcription/Solution/Exercism.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+public static class Complement
+{
+    public static string OfDna(string strand)
+    {
+        StringBuilder sb = new StringBuilder();
+        foreach (char n in strand)
+        {
+            switch (n)
+            {
+                case 'G':
+                    sb.Append('C');
+                    break;
+                case 'C':
+                    sb.Append('G');
+                    break;
+                case 'T':
+                    sb.Append('A');
+                    break;
+                case 'A':
+                    sb.Append('U');
+                    break;
+                default:
+                    throw new ArgumentException($"The nucleotide {n} is unknown and cannot be mapped.");
+            }
+
+        }
+        return sb.ToString();
+    }
+}
