8c3d895085c53023fa49f5eba37864caf0f1e25d
Tedford <dev@tedfordjohnson.com>
2017-02-14 07:53:09 -0500

    [F#] Largest Series Product

---
+++ fsharp/largest-series-product/Solution/Exercism.fs
@@ -0,0 +1,11 @@
+﻿module LargestSeriesProduct
+
+let largestProduct encodedDigits window = 
+    let digits = encodedDigits |> Seq.map (fun c-> int (c.ToString())) |> Seq.toArray
+    let max = Seq.length digits - window
+    [0..max]
+    |> List.map (fun i -> 
+        let endI = i + window - 1
+        digits.[i..endI] |> Array.fold (fun acc digit -> acc * digit) 1
+        )
+    |> List.max
