545143bb26d4ea3131e353b41dc22887580f12bb
Tedford <dev@tedfordjohnson.com>
2017-02-15 13:03:51 -0500

    [F#] Pythagorean Triplet
    Revised Solution

--- fsharp/pythagorean-triplet/Solution/Exercism.fs
+++ fsharp/pythagorean-triplet/Solution/Exercism.fs
@@ -3,18 +3,18 @@
 let triplet a b c = (a,b,c)
 
 let isPythagorean (a,b,c) =
-    let sorted = seq {yield a; yield b; yield c} |> Seq.sort |> Seq.toArray
+    let sorted = [| a; b; c|] |> Array.sort 
     let a' = sorted.[0]
     let b' = sorted.[1]
     let c' = sorted.[2]
-    
+
     pown a' 2 + pown b' 2 = pown c' 2
 
 let pythagoreanTriplets low high = 
-    [for a in [low .. high - 2] do
-        for b in [ a + 1 .. high - 1] do
-            for c in [b + 1 .. high] do
-                yield triplet a b c]
+    [low .. high - 2]
+    |> List.map (fun a -> [ a + 1 .. high - 1] |> List.map ( fun b -> [ b + 1 .. high ] |> List.map(fun c -> triplet a b c )) )
+    |> List.concat
+    |> List.concat
     |> List.filter isPythagorean
     
 
