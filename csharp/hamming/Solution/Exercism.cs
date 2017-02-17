718486f16cc453cb89d88989b186d35c6f8163e6
Tedford <dev@tedfordjohnson.com>
2017-02-17 10:52:02 -0500

    [C#] Hamming
    Problem and Solution

---
+++ csharp/hamming/Solution/Exercism.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+public static class Hamming
+{
+    public static int Compute(string sequence1, string sequence2)
+    {
+        return sequence1
+                .ToCharArray()
+                .Zip(sequence2.ToCharArray(), (l, r) => new { L = l, R = r })
+                .Aggregate(0, (total, current) => total + (current.L == current.R ? 0 : 1));
+    }
+}
