e505fc20901faacf182836cb2dd0e63a9635d01b
Tedford <dev@tedfordjohnson.com>
2017-02-17 09:27:46 -0500

    [F#] Pascal's Triangle
    Problem and Solution

---
+++ fsharp/pascals-triangle/Solution/Exercism.fs
@@ -0,0 +1,15 @@
+﻿module PascalsTriangle
+
+
+let triangle row = 
+    let extractValue values index =
+        match index with
+        | -1 -> 0
+        | _ when index > List.length values - 1 -> 0
+        | x -> values.[x]
+
+    let generateRow previous r =
+        [0..r - 1] 
+        |> List.map( fun i -> extractValue previous (i-1) + extractValue previous i ) 
+    
+    List.unfold (fun previous -> if List.length previous = row + 1 then None else Some(previous, generateRow previous (List.length previous + 1)) ) [1]
