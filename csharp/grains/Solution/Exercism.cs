d1bd2d288d36c3dbff58cab34f541ca250ff5f8c
Tedford <dev@tedfordjohnson.com>
2017-02-17 12:04:45 -0500

    [C#] Grains
    Problem and Solution

---
+++ csharp/grains/Solution/Exercism.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+public static class Grains
+{
+    public static double Square(int number) => Math.Pow(2, number - 1);
+
+    public static double Total() => Enumerable.Range(1, 64).Sum(i => Square(i));
+}
+
