225bbbc1efc72cd87e8c40c89c1849b2d0795151
Tedford <dev@tedfordjohnson.com>
2017-02-20 08:37:46 -0500

    [C#] Raindrops
    Problem and Solution

---
+++ csharp/raindrops/Solution/Exercism.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+public static class Raindrops
+{
+    public static string Convert(int number)
+    {
+        StringBuilder sb = new StringBuilder();
+        if (number % 3 == 0)
+        {
+            sb.Append("Pling");
+        }
+        if (number % 5 == 0)
+        {
+            sb.Append("Plang");
+        }
+        if (number % 7 == 0)
+        {
+            sb.Append("Plong");
+        }
+        if (sb.Length == 0)
+        {
+            sb.Append(number);
+        }
+        return sb.ToString();
+    }
+}
