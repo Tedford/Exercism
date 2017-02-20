336c615d247b307dc6c52f7f92faed31d0c2e82a
Tedford <dev@tedfordjohnson.com>
2017-02-20 08:29:04 -0500

    [C#] Pangram
    Problem and Solution

---
+++ csharp/pangram/Solution/Exercism.cs
@@ -0,0 +1,8 @@
+﻿using System.Linq;
+
+public static class Pangram
+{
+    private static bool IsAsciiLetter(char c) => 'a' <= c && c <= 'z';
+    public static bool IsPangram(string phrase) => phrase.ToLower().Where(i => IsAsciiLetter(i)).GroupBy(i=>i).ToDictionary(i => i.Key).Count == 26;
+            
+}
