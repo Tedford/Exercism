872673299fb2056e1fe1bc84724590f43e5d1df0
Tedford <dev@tedfordjohnson.com>
2017-02-27 18:03:47 -0500

    [F#] Prime Factors
    Problem and Solution

---
+++ fsharp/prime-factors/Solution/Exercism.fs
@@ -0,0 +1,11 @@
+﻿module PrimeFactors
+
+
+let primeFactorsFor number = 
+    let rec reduce acc factor remainder =
+        match factor > remainder, remainder % factor with
+        | true, _ -> acc
+        | _, 0L -> reduce (factor :: acc) factor (remainder / factor)
+        | _ -> reduce acc (factor + 1L) remainder
+    reduce [] 2L number
+    |> List.rev
