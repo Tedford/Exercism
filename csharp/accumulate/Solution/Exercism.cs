395f90eed4b35eebf4adaa534bfc6ae76caefabd
Tedford <dev@tedfordjohnson.com>
2017-03-02 08:55:46 -0500

    [C#] Accumulate
    Problem and Solution

---
+++ csharp/accumulate/Solution/Exercism.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Exercism
+{
+    public static IEnumerable<U> Accumulate<T, U>(this IEnumerable<T> sequence, Func<T, U> action)
+    {
+        foreach (var item in sequence)
+        {
+            yield return action(item);
+        }
+    }
+}
