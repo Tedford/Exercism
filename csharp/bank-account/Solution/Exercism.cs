179b50ea330c9b32998319fee8593e586027256a
Tedford <dev@tedfordjohnson.com>
2017-03-03 12:28:52 -0500

    [C#] Bank Account
    Problem and Solution

---
+++ csharp/bank-account/Solution/Exercism.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+public class BankAccount
+{
+
+    private bool _opened;
+    private int _balance;
+
+    public int Balance
+    {
+        get
+        {
+            AssertOpen();
+            return _balance;
+        }
+        set
+        {
+            AssertOpen();
+            _balance = value;
+        }
+    }
+
+    private void AssertOpen()
+    {
+        if (!_opened)
+        {
+            throw new InvalidOperationException("The account is closed.  No activity is allowed.");
+        }
+    }
+
+    public void Open() => _opened = true;
+
+    public void Close() => _opened = false;
+
+
+    public void UpdateBalance(int amount)
+    {
+        AssertOpen();
+        Interlocked.Add(ref _balance, amount);
+    }
+
+}
