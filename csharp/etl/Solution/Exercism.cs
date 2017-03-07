8070c18d5e091baead362af0ed15d737366796b1
Tedford <dev@tedfordjohnson.com>
2017-03-07 09:35:31 -0500

    [C#] ETL
    Problem and Solution

---
+++ csharp/etl/Solution/Exercism.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+public static class ETL
+{
+    public static Dictionary<string,int> Transform(Dictionary<int,IList<string>> input)
+    {
+        var output = new Dictionary<string, int>();
+        foreach(var entry in input)
+        {
+            foreach(var letter in entry.Value)
+            {
+                output[letter.ToLower()] = entry.Key;
+            }
+        }
+
+        return output;
+    }
+}
