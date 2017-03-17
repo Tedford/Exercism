c10903e4527823dcc0db46dcca5fce15838b0402
Tedford <dev@tedfordjohnson.com>
2017-03-17 17:34:31 -0400

    [Go] Hello World
    Problem and Solution

---
+++ go/hello-world/hello_world.go
@@ -0,0 +1,10 @@
+// Package greeting should have a package comment that summarizes what it's about.
+// https://golang.org/doc/effective_go.html#commentary
+package greeting
+
+const testVersion = 4
+
+// HelloWorld needs a comment documenting it as package does.
+func HelloWorld() string {
+	return "Hello, World!"
+}
