94df0fc873400a168c886e9785d775828052c02a
Tedford <dev@tedfordjohnson.com>
2017-03-16 21:42:54 -0400

    [F#] Parallel Letter Frequency
    Problem and Solution

---
+++ fsharp/parallel-letter-frequency/Solution/Exercism.fs
@@ -0,0 +1,28 @@
+﻿module ParallelLetterFrequency
+
+open System
+
+
+let mergeResults (results:seq<char*int> []) =
+    results
+    |> Seq.collect id
+    |> Seq.groupBy fst
+    |> Seq.map (fun (key, counts) -> (key, counts |> Seq.sumBy snd ))
+    |> Map.ofSeq
+
+
+let analyzeText phrase =
+    phrase.ToString().ToLowerInvariant()
+    |> Seq.filter Char.IsLetter
+    |> Seq.groupBy id
+    |> Seq.map (fun (key, values)-> (key, Seq.length values))
+
+let asyncAnalyzeText phrase =
+    async { return analyzeText phrase }
+
+let frequency phrases =
+    phrases
+    |> List.map asyncAnalyzeText
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    |> mergeResults
