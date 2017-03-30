dbfd3607a559265b44ad6afd7b881a601d3ed9ae
Tedford <dev@tedfordjohnson.com>
2017-03-29 21:02:20 -0400

    [F#] Atbash Cipher
    Problem and Solution

---
+++ fsharp/atbash-cipher/Solution/Exercism.fs
@@ -0,0 +1,17 @@
+﻿module Atbash
+open System
+
+let encode plaintext = 
+    let cipherAlphabet = [0..25] |> List.map (fun i-> int 'z' - i |> char) |> Array.ofList
+
+    let encodeChar = function
+        | d when Char.IsDigit d -> d
+        | l when Char.IsLetter l -> cipherAlphabet.[int l - int 'a']
+        | _ -> failwith "Invalid input"
+
+    plaintext.ToString().ToLower()
+    |> Seq.filter Char.IsLetterOrDigit
+    |> Seq.map encodeChar
+    |> Seq.chunkBySize 5
+    |> Seq.map String
+    |> String.concat " "
