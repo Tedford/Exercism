28c940329f7604a1e56fb9d17d1b8b1942f55c8a
Tedford <dev@tedfordjohnson.com>
2017-03-31 00:33:39 -0400

    [F#] Grep
    Problem and Solution

---
+++ fsharp/grep/Solution/Exercism.fs
@@ -0,0 +1,72 @@
+﻿module Grep
+
+open System
+open System.IO
+open System.Text.RegularExpressions
+
+type flags = 
+    | PrintLineNumber
+    | PrintFileNameOnly
+    | CaseInsensitive
+    | ShowCompliment
+    | MatchWholeLine
+
+let mapFlags flags = 
+    let getOpts = function
+    | "-n" -> Some(PrintLineNumber)
+    | "-l" -> Some(PrintFileNameOnly)
+    | "-i" -> Some(CaseInsensitive)
+    | "-v" -> Some(ShowCompliment)
+    | "-x" -> Some(MatchWholeLine)
+    | _ as x -> None
+
+    flags
+    |> Seq.map getOpts
+    |> Seq.choose id
+
+
+let processFile flags pattern lines =
+
+    let checkFlag option = flags |> Seq.contains option
+
+    let re = 
+        match checkFlag CaseInsensitive, checkFlag MatchWholeLine with
+        | true, true -> sprintf "(?i)^%s$" pattern
+        | true, false -> sprintf "(?i)%s" pattern
+        | false, true -> sprintf "^%s$" pattern
+        | _ -> pattern
+        |> Regex
+    
+    let matcher = if checkFlag ShowCompliment then fun line -> re.IsMatch(line) |> not else fun line -> re.IsMatch(line)
+
+    let filenamePrinter file text = sprintf "%s:%s" file text
+    let lineNumberPrinter index text = sprintf "%d:%s" index text
+    
+    seq  {
+        yield! lines
+        |> Seq.map (fun (fileCount, file, index, line) -> 
+            match matcher line with 
+            | true -> 
+                match checkFlag PrintFileNameOnly, fileCount > 1 ,checkFlag PrintLineNumber with
+                | true, _, _ -> Some(file+ "\n")
+                | false, true, true -> Some(line |> lineNumberPrinter index |> filenamePrinter file)
+                | false, true, false -> Some( line |> filenamePrinter file)
+                | false, false, true -> Some(line |> lineNumberPrinter index)
+                | _ -> Some(line)
+            | false -> None )
+    }
+    |> Seq.choose id
+    |> Seq.distinct
+
+
+let grep pattern flags files = 
+
+    let f = mapFlags (flags.ToString().Split([|' '|], StringSplitOptions.RemoveEmptyEntries))
+        
+    files
+    |> List.map ( fun file -> File.ReadAllLines(file) |> Array.mapi (fun index line -> Seq.length files, file, index+1, line+"\n"))
+    |> List.map (fun t -> processFile f pattern t)
+    |> Seq.concat
+    |> String.concat ""
+
+    
