1d34ebd6b9f31c6a0c21c644807e062956af365d
Tedford <dev@tedfordjohnson.com>
2017-04-02 19:25:31 -0400

    [C#] Queen Attack
    Problem and Solution

---
+++ csharp/queen-attack/QueenAttack.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Queen
+{
+    public Queen(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+
+    public int Row { get; }
+    public int Column { get; }
+}
+
+public static class Queens
+{
+    public static bool CanAttack(Queen white, Queen black)
+    {
+        if(white.Row == black.Row && white.Column == black.Column)
+        {
+            throw new ArgumentException($"The spot {white.Column}, {white.Row} is already occupied.");
+        }
+
+        return white.Row == black.Row || white.Column == black.Column || Math.Abs(white.Column - black.Column) == Math.Abs(white.Row - black.Row);
+    }
+
+}
