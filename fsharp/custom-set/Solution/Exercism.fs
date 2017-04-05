6f64cdf724921e62ca4549ad305766be48fb65ff
Tedford <dev@tedfordjohnson.com>
2017-04-05 00:42:35 -0400

    [F#] Custom Set
    Problem and Work in Progress

---
+++ fsharp/custom-set/Solution/Exercism.fs
@@ -0,0 +1,26 @@
+﻿module CustomSet
+
+let empty = []
+
+let isEmpty set = set |> List.length |> (>) 0
+
+let singleton value = []
+
+
+let contains value set = false
+
+let fromList values = []
+
+
+let isSubsetOf set1 set2 = false
+
+let isDisjointFrom set1 set2 = false
+
+
+let insert value set = []
+
+let intersection set1 set2 = []
+
+let difference set1 set2 = []
+
+let union set1 set2  = []
