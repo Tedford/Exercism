1f5008a99e46431bb2f7f5424c58664e38c34346
Tedford <dev@tedfordjohnson.com>
2017-04-09 19:41:34 -0400

    [F#] Custom Set \ Problem and Solution

--- fsharp/custom-set/CustomSetTest.fs
+++ fsharp/custom-set/CustomSetTest.fs
@@ -10,164 +10,164 @@ let ``Sets with no elements are empty`` () =
     Assert.That(isEmpty actual, Is.True)
 
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Sets with elements are not empty`` () =
     let actual = singleton 1
     Assert.That(isEmpty actual, Is.False)
 
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Nothing is contained in an empty set`` () =
     let actual = empty
     Assert.That(contains 1 actual, Is.False)
 
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Detect if the element is in the set`` () =
     let actual = fromList [1; 2; 3]
     Assert.That(contains 1 actual, Is.True)
 
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Detect if the element is not in the set`` () =
     let actual = fromList [1; 2; 3]
     Assert.That(contains 4 actual, Is.False)
     
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Empty set is a subset of another empty set`` () =
     let left = empty
     let right = empty
     Assert.That(isSubsetOf left right, Is.True)
     
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Empty set is a subset of non-empty set`` () =
     let left = empty
     let right = singleton 1
     Assert.That(isSubsetOf left right, Is.True)
     
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Non-empty set is not a subset of empty set`` () =
     let left = singleton 1
     let right = empty
     Assert.That(isSubsetOf left right, Is.False)
     
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Set is a subset of set with exact same elements`` () =
     let left = fromList [1; 2; 3]
     let right = fromList [1; 2; 3]
     Assert.That(isSubsetOf left right, Is.True)
     
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Set is a subset of larger set with same elements`` () =
     let left = fromList [1; 2; 3]
     let right = fromList [4; 1; 2; 3]
     Assert.That(isSubsetOf left right, Is.True)
     
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Set is not a subset of set that does not contain its elements`` () =
     let left = fromList [1; 2; 3]
     let right = fromList [4; 1; 3]
     Assert.That(isSubsetOf left right, Is.False)
         
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``The empty set is disjoint with itself`` () =
     let left = empty
     let right = empty
     Assert.That(isDisjointFrom left right, Is.True)    
     
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Empty set is disjoint with non-empty set`` () =
     let left = empty
     let right = singleton 1
     Assert.That(isDisjointFrom left right, Is.True)
     
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Non-empty set is disjoint with empty set`` () =
     let left = singleton 1
     let right = empty
     Assert.That(isDisjointFrom left right, Is.True)
     
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Sets are not disjoint if they share an element`` () =
     let left = fromList [1; 2]
     let right = fromList [2; 3]    
     Assert.That(isDisjointFrom left right, Is.False)
     
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Sets are disjoint if they share no elements`` () =
     let left = fromList [1; 2]
     let right = fromList [3; 4]
     Assert.That(isDisjointFrom left right, Is.True)
 
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Empty sets are equal`` () =
     let left = empty
     let right = empty
     Assert.That((left = right))
 
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Empty set is not equal to non-empty set`` () =
     let left = empty
     let right = fromList [1; 2; 3]
     Assert.That(left <> right)
 
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Non-empty set is not equal to empty set`` () =
     let left = fromList [1; 2; 3]
     let right = empty
     Assert.That(left <> right)
 
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Sets with the same elements are equal`` () =
     let left = fromList [1; 2]
     let right = fromList [2; 1]
     Assert.That((left = right))
 
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Sets with different elements are not equal`` () =
     let left = fromList [1; 2; 3]
     let right = fromList [1; 2; 4]
     Assert.That(left <> right)
 
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Add to empty set`` () =
     let actual = empty |> insert 3
     let expected = fromList [3]
     Assert.That(actual, Is.EqualTo(expected))
 
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Add to non-empty set`` () =
     let actual = fromList [1; 2; 4] |> insert 3
     let expected = fromList [1; 2; 3; 4]
     Assert.That(actual, Is.EqualTo(expected))
 
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Adding an existing element does not change the set`` () =
     let actual = fromList [1; 2; 3] |> insert 3
     let expected = fromList [1; 2; 3]
     Assert.That(actual, Is.EqualTo(expected))
             
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Intersection of two empty sets is an empty set`` () =
     let left = empty
     let right = empty
@@ -175,7 +175,7 @@ let ``Intersection of two empty sets is an empty set`` () =
     Assert.That(intersection left right = expected)
 
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Intersection of an empty set and non-empty set is an empty set`` () =
     let left = empty
     let right = fromList [3; 2; 5]
@@ -183,7 +183,7 @@ let ``Intersection of an empty set and non-empty set is an empty set`` () =
     Assert.That(intersection left right = expected)
 
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Intersection of a non-empty set and an empty set is an empty set`` () =
     let left = fromList [1; 2; 3; 4]
     let right = empty
@@ -191,7 +191,7 @@ let ``Intersection of a non-empty set and an empty set is an empty set`` () =
     Assert.That(intersection left right = expected)
 
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Intersection of two sets with no shared elements is an empty set`` () =
     let left = fromList [1; 2; 3]
     let right = fromList [4; 5; 6]
@@ -199,7 +199,7 @@ let ``Intersection of two sets with no shared elements is an empty set`` () =
     Assert.That(intersection left right = expected)
 
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Intersection of two sets with shared elements is a set of the shared elements`` () =
     let left = fromList [1; 2; 3; 4]
     let right = fromList [3; 2; 5]
@@ -207,7 +207,7 @@ let ``Intersection of two sets with shared elements is a set of the shared eleme
     Assert.That(intersection left right = expected)
             
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Difference of two empty sets is an empty set`` () =
     let left = empty
     let right = empty
@@ -215,7 +215,7 @@ let ``Difference of two empty sets is an empty set`` () =
     Assert.That(difference left right = expected)
 
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Difference of an empty set and non-empty set is an empty set`` () =
     let left = empty
     let right = fromList [3; 2; 5]
@@ -223,7 +223,7 @@ let ``Difference of an empty set and non-empty set is an empty set`` () =
     Assert.That(difference left right = expected)
 
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Difference of a non-empty set and an empty set is an empty set`` () =
     let left = fromList [1; 2; 3; 4]
     let right = empty
@@ -231,7 +231,7 @@ let ``Difference of a non-empty set and an empty set is an empty set`` () =
     Assert.That(difference left right = expected)
 
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Difference of two non-empty sets is a set of elements that are only in the first set`` () =
     let left = fromList [3; 2; 1]
     let right = fromList [2; 4]
@@ -239,7 +239,7 @@ let ``Difference of two non-empty sets is a set of elements that are only in the
     Assert.That(difference left right = expected)
            
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Union of two empty sets is an empty set`` () =
     let left = empty
     let right = empty
@@ -247,7 +247,7 @@ let ``Union of two empty sets is an empty set`` () =
     Assert.That(union left right = expected)
 
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Union of an empty set and non-empty set is the non-empty set`` () =
     let left = empty
     let right = fromList [2]
@@ -255,7 +255,7 @@ let ``Union of an empty set and non-empty set is the non-empty set`` () =
     Assert.That(union left right = expected)
 
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Union of a non-empty set and empty set is the non-empty set`` () =
     let left = fromList [1; 3]
     let right = empty
@@ -263,7 +263,7 @@ let ``Union of a non-empty set and empty set is the non-empty set`` () =
     Assert.That(union left right = expected)
 
 [<Test>]
-
+[<Ignore("Remove to run test")>]
 let ``Union of non-empty sets contains all unique elements`` () =
     let left = fromList [1; 3]
     let right = fromList [2; 3]
--- fsharp/custom-set/Solution/Exercism.fs
+++ fsharp/custom-set/Solution/Exercism.fs
@@ -2,25 +2,37 @@
 
 let empty = []
 
-let isEmpty set = set |> List.length |> (>) 0
+let isEmpty set = set |> List.isEmpty
 
-let singleton value = []
+let singleton value = [value]
 
+let contains value set = set |> List.contains value
 
-let contains value set = false
+let fromList values = values |> List.sort
 
-let fromList values = []
+let isSubsetOf set1 set2 = 
+    [for e in set1 -> List.contains e set2]
+    |> Seq.forall ( fun exists -> exists = true )
 
+let isDisjointFrom set1 set2 = 
+    [for e in set1 -> List.contains e set2]
+    |> Seq.forall ( fun exists -> exists = false )
 
-let isSubsetOf set1 set2 = false
+let insert value set=
+    if contains value set then set else value :: set |> List.sort
 
-let isDisjointFrom set1 set2 = false
+let intersection set1 set2 = 
+    [for e in set1 -> e, List.contains e set2]
+    |> List.filter (fun (_, intersects) -> intersects) 
+    |> List.map fst
 
+let difference set1 set2 =
+    [for e in set1 -> e, List.contains e set2]
+    |> List.filter (fun (_, intersects) -> not intersects) 
+    |> List.map fst
 
-let insert value set = []
-
-let intersection set1 set2 = []
-
-let difference set1 set2 = []
-
-let union set1 set2  = []
+let union set1 set2  = 
+    set1 @ set2
+    |> List.groupBy id
+    |> List.map fst
+    |> List.sort
