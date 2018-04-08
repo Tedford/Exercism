db81c92d9f3a2e91d672b06a5008236649924fc9
Tedford <dev@tedfordjohnson.com>
2018-04-08 17:23:37 -0400

    exercise solution

--- fsharp/crypto-square/Solution/Exercism.fs
+++ fsharp/crypto-square/Solution/Exercism.fs
@@ -1,5 +1,7 @@
 ﻿module CryptoSquare
 
+open System
+open System.Linq
 open System.Text.RegularExpressions
 
 let normalizePlaintext input = Regex.Replace(input.ToString().ToLower(),"[^a-z0-9]","")
@@ -23,6 +25,36 @@ let plaintextSegments input =
     |> Seq.toList
 
 
-let ciphertext input = ""
+let ciphertext input = 
+    let normalized = normalizePlaintext input
+    let normalizedLength = Seq.length normalized
+    let r = size input
+    
+    let slices = normalizedLength - (pown r 2) + r
+    
+    [|for i in [0..(slices-1)] do
+        for c in [0..r] do
+            yield normalized.ElementAtOrDefault(c * r + i)
+    |]
+    |> Array.filter Char.IsLetter
+    |> String
 
-let normalizeCiphertext input = ""
+let normalizeCiphertext input =
+    let normalized = normalizePlaintext input
+    let normalizedLength = Seq.length normalized
+    let r = size input
+    let remainder = int (normalizedLength - r * r)
+
+    let r2 = r - remainder
+    let changepoint = normalizedLength - (pown r2 2)
+    
+    let code = ciphertext normalized
+
+    let slices = (List.init (changepoint / r) (fun _ -> r)) @ (List.init ((normalizedLength - changepoint) / r2 ) (fun _ -> r2))
+    
+    Seq.unfold (fun (s:string,lengths) -> 
+        match lengths with 
+        | [] -> None
+        | x::xs -> Some( (s.Substring(0, x), []), (s.Substring(x), xs))
+        ) (code, slices)
+    |> Seq.map fst
---
+++ fsharp/two-fer/TwoFer.fs
@@ -0,0 +1,6 @@
+﻿module TwoFer
+
+let name (input: string option): string = 
+    match input with
+    | None -> "One for you, one for me."
+    | Some(X) -> sprintf "One for %s, one for me." X
