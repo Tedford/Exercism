c6d78d75f8650fc59c86b715e702e6845fc83e34
Tedford <dev@tedfordjohnson.com>
2018-04-14 20:40:25 -0400

    Solution to F# - Collatz Conjecture

--- fsharp/collatz-conjecture/CollatzConjecture.fs
+++ fsharp/collatz-conjecture/CollatzConjecture.fs
@@ -1,3 +1,16 @@
 ﻿module CollatzConjecture
 
-let steps (number: int): int option = failwith "You need to implement this function."
+let even(n) = n / 2
+
+let odd(n) = 3 * n + 1
+
+let next(n: int) = if n % 2 = 0 then even(n) else odd(n)
+
+let steps (number: int): int option =
+    match number < 1 with
+    | true -> None
+    | false -> 
+        Seq.unfold(fun state -> if state <= 1 then None else Some(state, next state)) number 
+        |> Seq.length 
+        |> Some
+    
