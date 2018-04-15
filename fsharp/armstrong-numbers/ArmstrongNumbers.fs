678084e976fc87559a252f3feb40219c551d8262
Tedford <dev@tedfordjohnson.com>
2018-04-15 01:12:23 -0400

    Solution F# - Armstrong Numbers

--- fsharp/armstrong-numbers/ArmstrongNumbers.fs
+++ fsharp/armstrong-numbers/ArmstrongNumbers.fs
@@ -1,3 +1,11 @@
 ﻿module ArmstrongNumbers
+let toFloat c = c.ToString() |> float
 
-let isArmstrongNumber (number: int): bool = failwith "You need to implement this function."
+let isArmstrongNumber (number: int): bool = 
+    let s:string = sprintf "%d" number
+    let power = s.Length |> float
+    
+    let sum = s |> Seq.sumBy (fun c -> System.Math.Pow(toFloat c, power))
+    
+    number = (sum |> int)
+
