ae391db8e7a2974568f53c4127acf31c907c6be7
Tedford <dev@tedfordjohnson.com>
2018-11-17 18:24:04 -0500

    Solution - C# - Reverse String

--- csharp/reverse-string/ReverseString.cs
+++ csharp/reverse-string/ReverseString.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public static class ReverseString
 {
-    public static string Reverse(string input)
-    {
-        throw new NotImplementedException("You need to implement this function.");
+    public static string Reverse(string input) => string.IsNullOrEmpty(input) ? input: String.Join(String.Empty,ReverseKernel(input));
+    private static IEnumerable<char> ReverseKernel(string input){
+        for(int i= input.Length-1; i > -1; i--){
+            yield return input[i];
+        }
     }
 }
