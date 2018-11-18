79b36ef0e2718d9e8b05643ca933a85a22559c5c
Tedford <dev@tedfordjohnson.com>
2018-11-18 15:32:15 -0500

    Solution - F# - Darts

--- fsharp/darts/Darts.fs
+++ fsharp/darts/Darts.fs
@@ -1,3 +1,8 @@
 ﻿module Darts
 
-let score (x: double) (y: double): int = failwith "You need to implement this function."
+let score (x: double) (y: double): int =
+    match x,y with
+    | _ when x <= 1.0 && y <= 1.0 -> 10
+    | _ when x <= 5.0 && y <= 5.0 -> 5
+    | _ when x <= 10.0 && y <= 10.0 -> 1
+    | _ -> 0
--- fsharp/darts/DartsTest.fs
+++ fsharp/darts/DartsTest.fs
@@ -11,19 +11,19 @@ open Darts
 let ``A dart lands outside the target`` () =
     score 15.3 13.2 |> should equal 0
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``A dart lands just in the border of the target`` () =
     score 10.0 0.0 |> should equal 1
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``A dart lands in the middle circle`` () =
     score 3.0 3.7 |> should equal 5
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``A dart lands right in the border between outside and middle circles`` () =
     score 0.0 5.0 |> should equal 5
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``A dart lands in the inner circle`` () =
     score 0.0 0.0 |> should equal 10
 
