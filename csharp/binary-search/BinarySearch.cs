37267f1d783bc83aa13139c32fd03ca1c7beeb0d
Tedford <dev@tedfordjohnson.com>
2018-11-21 21:15:27 -0500

    Solution - C# - Binary Search

--- csharp/binary-search/BinarySearch.cs
+++ csharp/binary-search/BinarySearch.cs
@@ -1,9 +1,36 @@
 ﻿using System;
+using System.Linq;
 
 public static class BinarySearch
 {
     public static int Find(int[] input, int value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+	input = input ?? new int[0];
+	var index = 0;
+
+	while(input.Any())
+	{
+		int current = input.Length / 2;
+		if (input[current] < value)
+		{
+			int size = input.Length - Math.Max(current,1);
+			var array2 = new int[size];
+			Array.Copy(input, current, array2, 0, size);
+			input = array2;
+			index += current;
+		}
+		else if (input[current] > value)
+		{
+			var array2 = new int[current];
+			Array.Copy(input, array2, current);
+			input = array2;
+		}
+		else 
+		{
+			return index + current;
+		}
+	}
+
+	return -1;
     }
 }
