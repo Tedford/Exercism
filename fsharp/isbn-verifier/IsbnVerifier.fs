52eed4ed70902e29eaa9a75d234b0086ab68d9da
Tedford <dev@tedfordjohnson.com>
2018-11-23 17:41:42 -0500

    Solution - F# - ISBN Verifier

--- fsharp/isbn-verifier/IsbnVerifier.fs
+++ fsharp/isbn-verifier/IsbnVerifier.fs
@@ -1,3 +1,26 @@
 ﻿module IsbnVerifier
+open System
 
-let isValid isbn = failwith "You need to implement this function."
+
+let encode c = 
+    match Char.IsDigit(c), c with
+    | true,_ -> Int32.Parse(c.ToString())
+    | false, 'x' ->  10
+    | false, _ -> failwith "invalid encoding detected"
+
+let isValid isbn = 
+    let isbn'= isbn.ToString().Replace("-","").ToLower()
+
+    match isbn'.Length with
+    | 10 ->
+        try
+            let sum = 
+                isbn' 
+                |> Seq.mapi (fun index digit -> 
+                    if digit = 'x' && index <> 9 then
+                        failwith "The check digit must be last"
+                    (10 - index) * encode digit) 
+                |> Seq.sum
+            sum % 11 = 0
+        with | _ -> false 
+    | _ -> false    
