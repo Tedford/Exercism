e15843099907946e31b0211bd326f8a39b19bcb8
Tedford <dev@tedfordjohnson.com>
2018-11-23 17:06:27 -0500

    Solution - F# - High Scores

--- fsharp/high-scores/HighScores.fs
+++ fsharp/high-scores/HighScores.fs
@@ -1,11 +1,21 @@
 ﻿module HighScores
 
-let scores (values: int list): int list = failwith "You need to implement this function."
+let scores (values: int list): int list = values
 
-let latest (values: int list): int = failwith "You need to implement this function."
+let latest (values: int list): int = values |> List.rev |> List.head
 
-let highest (values: int list): int = failwith "You need to implement this function."
+let highest (values: int list): int = 
+    values 
+    |> List.sortByDescending id
+    |> List.head
 
-let top  (values: int list): int list = failwith "You need to implement this function."
+let top  (values: int list): int list = values |> List.sortByDescending id |> List.truncate 3
 
-let report (values: int list): string = failwith "You need to implement this function."
+let report (values: int list): string =
+    let highScore = highest values
+    let latestScore = latest values
+
+    if latestScore >= highScore then
+        sprintf "Your latest score was %d. That's your personal best!" highScore
+    else 
+       sprintf "Your latest score was %d. That's %d short of your personal best!" latestScore (highScore-latestScore)
