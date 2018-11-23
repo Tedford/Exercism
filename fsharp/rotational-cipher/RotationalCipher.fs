fdffbbc98990bea4ce925af5e25944df7144454b
Tedford <dev@tedfordjohnson.com>
2018-11-23 18:47:22 -0500

    Solution - F# - Rotational Cipher

--- fsharp/rotational-cipher/RotationalCipher.fs
+++ fsharp/rotational-cipher/RotationalCipher.fs
@@ -1,3 +1,22 @@
 ﻿module RotationalCipher
-
-let rotate shiftKey text = failwith "You need to implement this function."
+open System
+let buildCipher offset initial =
+    [0..25]
+    |> List.map (fun n -> char ((n + offset) % 26  + int initial))
+    |> List.map char
+let charToIndex initial c = int c - int initial 
+let capIndex = charToIndex 'A'
+let lowerIndex = charToIndex 'a'
+let rotate shiftKey text = 
+    let lowerCipher = buildCipher shiftKey 'a'
+    let capCipher = buildCipher shiftKey 'A'
+    
+    text
+    |> Seq.map (fun c -> 
+        match Char.IsLetter(c), Char.IsUpper(c) with
+        | true, true -> capCipher.[capIndex c]
+        | true, false -> lowerCipher.[lowerIndex c]
+        | false, _ -> c
+        )
+    |> Seq.toList
+    |> String.Concat
