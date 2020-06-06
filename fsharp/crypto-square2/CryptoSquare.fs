caa912dcb0d34717ebdf0e296e93ce806853208a
Tedford <dev@tedfordjohnson.com>
2020-06-06 18:37:11 -0400

    working on solution - CryptoSquare F#

--- fsharp/crypto-square2/CryptoSquare.fs
+++ fsharp/crypto-square2/CryptoSquare.fs
@@ -2,4 +2,22 @@
 
 open System
 
-let ciphertext input = failwith "You need to implement this function."
+let ciphertext input =
+    let normalized = input.ToString().ToLower().Replace(" ", "")
+    let count = normalized.Length |> float
+    let c = Math.Ceiling(count / 2.0) |> int
+    let r = Math.Floor(count / 2.0) |> int
+
+    let pad = r*c - normalized.Length
+
+    let normalized = input + new String(' ',pad)
+
+    let chars = 
+        seq { for r' in 0 .. r  -> 
+                seq { for c' in 0 .. c -> 
+                        normalized.[c' + r' * r]
+                }   
+        }
+
+    ""
+    
