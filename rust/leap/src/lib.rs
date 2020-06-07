c84181c6df0c40cfbdec86d5717e3c7c1caf181e
Tedford <dev@tedfordjohnson.com>
2020-06-07 12:51:24 -0400

    solution - Rust - Leap

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,3 +1,7 @@
 pub fn is_leap_year(year: u64) -> bool {
-    unimplemented!("true if {} is a leap year", year)
+    let is_mod4 = year % 4 == 0;
+    let is_divisible_by_100 = year % 100 == 0;
+    let is_divisible_by_400 = year % 400 == 0;
+
+    is_mod4 && (!is_divisible_by_100 || is_divisible_by_400)
 }
