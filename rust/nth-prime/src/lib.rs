9d93019f1f897049880e1fdb9b18680842caac0f
Tedford <dev@tedfordjohnson.com>
2020-06-07 15:33:37 -0400

    solution - Rust - Nth Prime

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,3 +1,45 @@
 pub fn nth(n: u32) -> u32 {
-    unimplemented!("What is the 0-indexed {}th prime number?", n)
+
+    let prime_0 = 2;
+    let mut last_prime = prime_0;
+    let mut found =0;
+    let mut i = 0;
+
+    while found <= n
+    {
+        let mut candidate = prime_0 + i;
+        if is_prime(candidate)
+        {
+            last_prime = candidate;
+            found += 1;
+        }
+        i += 1;
+    }
+
+    last_prime
+}
+
+pub fn is_prime(n: u32) -> bool
+{
+    if n <= 3 
+    {
+        return n > 1;
+    }
+    else if n % 2 == 0 || n % 3 == 0
+    {
+        return false;
+    }
+    
+    let mut i = 5;
+
+    while i * i <= n 
+    {
+        if n % i == 0 || n % (i + 2) == 0
+        {
+            return false;
+        }
+        i += 6;
+    }
+
+    true
 }
