e6eca1f0b62387e4124d41dd7bd597d6bfccfd08
Tedford <dev@tedfordjohnson.com>
2020-06-07 17:29:06 -0400

    Solution - Rust - Armstrong Numbers

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,3 +1,9 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    let s= num.to_string();
+    let digits = s.chars().map(|c| c.to_digit(10).unwrap());
+    let digit_count = s.chars().count() as u32;
+
+    let sum : u32 = digits.map(|d| d.pow(digit_count)).sum();
+
+    sum == num
 }
