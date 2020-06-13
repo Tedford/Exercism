80a6c10f6089574d6b38d9738a68be769874c49a
Tedford <dev@tedfordjohnson.com>
2020-06-13 13:29:42 -0400

    Solution - Rust - gigasecond

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,6 +1,8 @@
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Utc, Duration};
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    unimplemented!("What time is a gigasecond later than {}", start);
+    let base : i64 = 10;
+    let billion = base.pow(9);
+    start + Duration::seconds(billion)
 }
