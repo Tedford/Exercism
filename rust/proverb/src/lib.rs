50f7cf25d2d4acabd0c5286949a2e8117b227e70
Tedford <dev@tedfordjohnson.com>
2020-06-13 12:24:02 -0400

    Feedback - Rust - idomatic refactor

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -2,17 +2,13 @@ pub fn build_proverb(list: &[&str]) -> String {
     if list.is_empty() {
         String::new()
     } else {
-        let mut verse = String::new();
-
-        for i in 0..(list.len() - 1) {
-            verse.push_str(&format!(
-                "For want of a {} the {} was lost.\n",
-                list[i],
-                list[i + 1]
-            ));
-        }
-
-        verse.push_str(&format!("And all for the want of a {}.", list[0]));
-        verse
+        list.windows(2)
+            .map(|slice| format!("For want of a {} the {} was lost.", slice[0], slice[1]))
+            .chain(std::iter::once(format!(
+                "And all for the want of a {}.",
+                list[0]
+            )))
+            .collect::<Vec<_>>()
+            .join("\n")
     }
 }
