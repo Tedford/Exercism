6690d75df141cd7411da0d6380523997ca8d2ade
Tedford <dev@tedfordjohnson.com>
2020-06-20 11:16:20 -0400

    Review - Rust - Gigasecond

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,8 +1,8 @@
 use chrono::{DateTime, Duration, Utc};
 
+const BILLON: i64 = 1_000_000_000;
+
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    let base: i64 = 10;
-    let billion = base.pow(9);
-    start + Duration::seconds(billion)
+    start + Duration::seconds(BILLON)
 }
