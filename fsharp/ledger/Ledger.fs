f568348bdde4e1d9d1b0c94948bf0d737697a1d1
Tedford <dev@tedfordjohnson.com>
2020-07-03 20:04:18 -0400

    Solution - F# - Ledger

--- fsharp/ledger/Ledger.fs
+++ fsharp/ledger/Ledger.fs
@@ -2,60 +2,47 @@
 
 open System
 open System.Globalization
+open System.Text
+
+let currencyFormat ="#,#0.00"
+let enUS = CultureInfo("en-US")
+let nlNL = CultureInfo("nl-NL")
 
 type Entry = { dat: DateTime; des: string; chg: int }
 
 let mkEntry date description change = { dat = DateTime.Parse(date, CultureInfo.InvariantCulture); des = description; chg = change }
-        
+
 let formatLedger currency locale entries =
+
+    let dateFormat = match locale with "en-US" -> "MM\/dd\/yyyy" | "nl-NL" -> "dd-MM-yyyy" | x -> failwithf "Unrecognized locale %s specified" x
+    let symbol = match currency with "USD" -> "$" | "EUR" -> "€" | x -> failwithf "Unrecognized currency %s specified" x
+    let header = match locale with "en-US" -> "Date       | Description               | Change       " | "nl-NL" -> "Datum      | Omschrijving              | Verandering  " | x -> failwithf "Recognized locale %s specified" x
     
-    let mutable res = ""
+    let sb = StringBuilder()
 
-    if locale = "en-US" then res <- res + "Date       | Description               | Change       "
-    if locale = "nl-NL" then res <- res + "Datum      | Omschrijving              | Verandering  "
-        
+    let positiveEntry (c:float) = 
+        match locale with 
+        | "en-US" -> sb.Append((sprintf "%s%s " symbol (c.ToString(currencyFormat, enUS))).PadLeft(13)) |> ignore
+        | "nl-NL" -> sb.Append((sprintf "%s %s " symbol (c.ToString(currencyFormat, nlNL))).PadLeft(13)) |> ignore
+        | x -> failwithf "Unexpected locale %s specified" x
+
+    let negativeEntry (c:float) = 
+        match locale with
+        | "en-US" -> sb.Append((sprintf "(%s%s)" symbol (c.ToString(currencyFormat, enUS).Substring(1))).PadLeft(13)) |> ignore
+        | "nl-NL"-> sb.Append((sprintf "%s %s" symbol (c.ToString(currencyFormat, nlNL))).PadLeft(13)) |> ignore
+        | x -> failwithf "Unexpected locale %s specified" x
+
+    sb.Append(header) |> ignore
+
     for x in List.sortBy (fun x -> x.dat, x.des, x.chg) entries do
+        
+        sb.AppendFormat("\n{0} | {1} | ", x.dat.ToString(dateFormat),  if x.des.Length > 25 then x.des.[0..21] + "..." else x.des.PadRight(25) ) |> ignore
 
-        res <- res + "\n"
-
-        if locale = "nl-NL" then 
-            res <- res + x.dat.ToString("dd-MM-yyyy")
-
-        if locale = "en-US" then 
-            res <- res + x.dat.ToString("MM\/dd\/yyyy")
-                
-        res <- res + " | "
-
-        if x.des.Length <= 25 then 
-            res <- res + x.des.PadRight(25)
-        elif x.des.Length = 25 then 
-            res <- res + x.des
-        else 
-            res <- res + x.des.[0..21] + "..."
-
-        res <- res + " | "
         let c = float x.chg / 100.0
 
-        if c < 0.0 then 
-            if locale = "nl-NL" then
-                if currency = "USD" then
-                    res <- res + ("$ " + c.ToString("#,#0.00", new CultureInfo("nl-NL"))).PadLeft(13) 
-                if currency = "EUR" then
-                    res <- res + ("€ " + c.ToString("#,#0.00", new CultureInfo("nl-NL"))).PadLeft(13) 
-            if locale = "en-US" then
-                if currency = "USD" then
-                    res <- res + ("($" + c.ToString("#,#0.00", new CultureInfo("en-US")).Substring(1) + ")").PadLeft(13) 
-                if currency = "EUR" then
-                    res <- res + ("(€" + c.ToString("#,#0.00", new CultureInfo("en-US")).Substring(1) + ")").PadLeft(13) 
-        else 
-            if locale = "nl-NL" then
-                if currency = "USD" then
-                    res <- res + ("$ " + c.ToString("#,#0.00", new CultureInfo("nl-NL")) + " ").PadLeft(13) 
-                if currency = "EUR" then
-                    res <- res + ("€ " + c.ToString("#,#0.00", new CultureInfo("nl-NL")) + " ").PadLeft(13) 
-            if locale = "en-US" then
-                if currency = "USD" then
-                    res <- res + ("$" + c.ToString("#,#0.00", new CultureInfo("en-US")) + " ").PadLeft(13) 
-                if currency = "EUR" then
-                    res <- res + ("€" + c.ToString("#,#0.00", new CultureInfo("en-US")) + " ").PadLeft(13) 
-    res
+        if c < 0.0 then
+            negativeEntry c
+        else
+            positiveEntry c
+
+    sb.ToString()
