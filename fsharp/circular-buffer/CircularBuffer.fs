9cb20fd600f9d80d5239bdb08d1739044ba9dc81
Tedford <dev@tedfordjohnson.com>
2020-07-18 18:27:51 -0400

    Solution - F# - Circular Buffer

--- fsharp/circular-buffer/CircularBuffer.fs
+++ fsharp/circular-buffer/CircularBuffer.fs
@@ -1,11 +1,33 @@
 ﻿module CircularBuffer
 
-let mkCircularBuffer size = failwith "You need to implement this function."
+open System
 
-let clear buffer = failwith "You need to implement this function."
+type Buffer = {
+    Size: int;
+    Elements : int list;
+}
 
-let write value buffer = failwith "You need to implement this function."
-        
-let forceWrite value buffer = failwith "You need to implement this function."
 
-let read buffer = failwith "You need to implement this function."
+let hasCapacity buffer = buffer.Elements |> List.length < buffer.Size
+
+let mkCircularBuffer size = 
+    {Size = size; Elements = []}
+
+let clear buffer = 
+    {buffer with Elements = []} 
+
+let write value buffer = 
+    match hasCapacity buffer  with
+    | true -> {buffer with Elements = buffer.Elements @ [value]}
+    | false -> raise (Exception("Cannot push new value, the buffer is full"))
+
+let forceWrite value buffer = 
+    match hasCapacity buffer, buffer.Elements with 
+    | false , _ :: tail  -> {buffer with Elements = tail @ [value]}
+    | true, x  -> {buffer with Elements = x @ [value]}
+    | _ -> failwith "Unexpected condition"
+
+let read buffer = 
+    match buffer.Elements with
+    | head:: tail -> (head, {buffer with Elements = tail})
+    | [] -> failwith "Attempting to read an empty buffer"
