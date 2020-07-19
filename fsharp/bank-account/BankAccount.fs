93461946a02b6b62f313dd4b33a13b5df68f8d55
Tedford <dev@tedfordjohnson.com>
2020-07-19 14:13:16 -0400

    Solution - F# - Bank Account

--- fsharp/bank-account/BankAccount.fs
+++ fsharp/bank-account/BankAccount.fs
@@ -1,11 +1,51 @@
 ﻿module BankAccount
 
-let mkBankAccount() = failwith "You need to implement this function."
+type Account(balance) =
 
-let openAccount account = failwith "You need to implement this function."
+    new () =
+        Account(None)
+        
+    member val Balance : Option<decimal> = balance with get, set
 
-let closeAccount account = failwith "You need to implement this function."
 
-let getBalance account = failwith "You need to implement this function."
+let mkBankAccount() = Account()
 
-let updateBalance change account = failwith "You need to implement this function."
+let openAccount (account:Account) = 
+    account.Balance <- Some(0.0m)
+    account
+
+let closeAccount account = Account()
+    
+
+let getBalance (account:Account) : Option<decimal> = account.Balance
+
+let updateBalance change (account:Account) = 
+    match account.Balance with
+    | None -> failwith "The account is not open"
+    | Some(x) ->  
+        account.Balance <- Some(x + change)
+        account
+        
+
+//type Account = {
+//    Balance : Option<decimal>
+//}
+
+//let mkBankAccount() = {Balance = None}
+
+//let openAccount (account:Account) = {Balance = Some(0.0m)}
+    
+
+//let closeAccount account = {Balance = None}
+
+//let getBalance account = account.Balance
+
+//let updateBalance change (account:Account) = 
+//    match account.Balance with
+//    | None -> failwith "The account is not open"
+//    | Some(x) ->  
+//    { account with Balance = Some(x + change)}
+//        //account.Balance <- Some(x + change)
+//        //account
+        
+        
