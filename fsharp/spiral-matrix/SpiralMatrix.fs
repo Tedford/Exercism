e5d1b8a13d381830238802d0f9d5cc5154ce10fc
Tedford <dev@tedfordjohnson.com>
2020-07-19 21:23:08 -0400

    Solution - F# - Spiral Matrix

--- fsharp/spiral-matrix/SpiralMatrix.fs
+++ fsharp/spiral-matrix/SpiralMatrix.fs
@@ -1,3 +1,32 @@
 ﻿module SpiralMatrix
 
-let spiralMatrix size = failwith "You need to implement this function."
+let spiralMatrix size = 
+
+    let matrix = Array2D.create size size 0
+
+    let mutable pos = 0
+    let mutable count = size
+    let mutable value = -size
+    let mutable sum = -1
+
+    while count > 0 do  
+        value <- -1 * value / size
+        [|1..count|]
+        |> Array.iter (fun _ ->
+            sum <- sum + value
+            pos  <- pos + 1
+            matrix.[sum / size, sum % size] <- pos
+            )
+
+        value <- value * size
+        count <- count - 1
+
+        [|1..count|]
+        |> Array.iter (fun _ ->
+            sum <- sum + value
+            pos <- pos + 1
+            matrix.[sum / size, sum % size] <- pos
+        )
+    
+    [1..size ]
+    |> List.map (fun row -> matrix.[row-1,*] |> Array.toList)
