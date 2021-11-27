c1c54337e27b41cf683fd45361be128c16b8bd81
Tedford <dev@tedfordjohnson.com>
2021-11-27 15:13:34 -0500

    refactor - python - triangle

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,34 +1,32 @@
 """exercise"""
 
-
-def equilateral(sides):
-    """
-    :param sides: length of sides
-    """
-    return is_triange(sides) and (sides[0] == sides[1]) and (sides[1] == sides[2])
-
-
-def isosceles(sides):
-    """
-    :param sides: length of sides
-    """
-    return is_triange(sides) and ((sides[0] == sides[1]) or (sides[1]==sides[2]) or (sides[0] == sides[2]))
-
-
-def scalene(sides):
-    """
-    :param sides: length of sides
-    """
-    return is_triange(sides) and sides[0] != sides[1] and sides[1] != sides[2] and sides[0] != sides[2]
-
-
-def is_triange(sides):
+def is_triange(test):
     """
     :param sides: length of sides
     :return: True if a triangle; otherwise, false
     """
-    sides.sort()
-    is_three_sides = len(sides) == 3
-    non_zero_length = all(length > 0 for length in sides)
-    not_degenerate = sides[0] + sides[1] > sides[2]
-    return  is_three_sides and  non_zero_length and not_degenerate
+    return lambda sides: all(length > 0 for length in sides) and 2*max(sides) < sum(sides) and test(sides)
+
+
+@is_triange
+def equilateral(sides):
+    """
+    :param sides: length of sides
+    """
+    return (sides[0] == sides[1]) and (sides[1] == sides[2])
+
+
+@is_triange
+def isosceles(sides):
+    """
+    :param sides: length of sides
+    """
+    return ((sides[0] == sides[1]) or (sides[1] == sides[2]) or (sides[0] == sides[2]))
+
+
+@is_triange
+def scalene(sides):
+    """
+    :param sides: length of sides
+    """
+    return sides[0] != sides[1] and sides[1] != sides[2] and sides[0] != sides[2]
