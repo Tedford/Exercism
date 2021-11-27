943f1c9b8162331a21919d475cd980f0a934c912
Tedford <dev@tedfordjohnson.com>
2021-11-27 14:06:53 -0500

    solution - python - tisbury treasure hunt

--- python/tisbury-treasure-hunt/tuples.py
+++ python/tisbury-treasure-hunt/tuples.py
@@ -1,3 +1,6 @@
+"""exercise"""
+
+
 def get_coordinate(record):
     """
 
@@ -5,7 +8,7 @@ def get_coordinate(record):
     :return: str - the extracted map coordinate.
     """
 
-    pass
+    return record[1]
 
 
 def convert_coordinate(coordinate):
@@ -15,7 +18,7 @@ def convert_coordinate(coordinate):
     :return:  tuple - the string coordinate seperated into its individual components.
     """
 
-    pass
+    return tuple(coordinate)
 
 
 def compare_records(azara_record, rui_record):
@@ -25,8 +28,8 @@ def compare_records(azara_record, rui_record):
     :param rui_record: tuple - a (location, coordinate, quadrant) trio.
     :return: bool - True if coordinates match, False otherwise.
     """
-
-    pass
+    rui_coordinate = rui_record[1][0] + rui_record[1][1]
+    return azara_record[1] == rui_coordinate
 
 
 def create_record(azara_record, rui_record):
@@ -37,7 +40,7 @@ def create_record(azara_record, rui_record):
     :return:  tuple - combined record, or "not a match" if the records are incompatible.
     """
 
-    pass
+    return azara_record + rui_record if compare_records(azara_record, rui_record) else "not a match"
 
 
 def clean_up(combined_record_group):
@@ -46,5 +49,22 @@ def clean_up(combined_record_group):
     :param combined_record_group: tuple of tuples - everything from both participants.
     :return: string of tuples separated by newlines - everything "cleaned". Excess coordinates and information removed.
     """
+    return '\n'.join(print_report(record) for _, record in enumerate(combined_record_group)) + '\n'
 
-    pass
+
+def print_report(record):
+    """
+    :param record: the combined tuple data
+    :return: the formatted string
+    """
+    azara_location, _, rui_location, rui_coordinate, quadrant = tuple(record)
+    return f'({quote(azara_location)}, {quote(rui_location)}, ({quote(rui_coordinate[0])}, {quote(rui_coordinate[1])}), {quote(quadrant)})'
+
+
+def quote(text):
+    """
+    :param s: the string value to enquote
+    :return: the enquoted string value
+    """
+
+    return f'"{text}"' if text.find("'") != -1 else f'\'{text}\''
