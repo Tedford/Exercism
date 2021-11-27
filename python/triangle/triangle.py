4b327abd45ff56429efde09421df3117b5e97e2f
Tedford <dev@tedfordjohnson.com>
2021-11-27 14:48:28 -0500

    solution - python - triangle

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,10 +1,34 @@
+"""exercise"""
+
+
 def equilateral(sides):
-    pass
+    """
+    :param sides: length of sides
+    """
+    return is_triange(sides) and (sides[0] == sides[1]) and (sides[1] == sides[2])
 
 
 def isosceles(sides):
-    pass
+    """
+    :param sides: length of sides
+    """
+    return is_triange(sides) and ((sides[0] == sides[1]) or (sides[1]==sides[2]) or (sides[0] == sides[2]))
 
 
 def scalene(sides):
-    pass
+    """
+    :param sides: length of sides
+    """
+    return is_triange(sides) and sides[0] != sides[1] and sides[1] != sides[2] and sides[0] != sides[2]
+
+
+def is_triange(sides):
+    """
+    :param sides: length of sides
+    :return: True if a triangle; otherwise, false
+    """
+    sides.sort()
+    is_three_sides = len(sides) == 3
+    non_zero_length = all(length > 0 for length in sides)
+    not_degenerate = sides[0] + sides[1] > sides[2]
+    return  is_three_sides and  non_zero_length and not_degenerate
