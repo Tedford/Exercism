d88a4941a6e0d856447c266a79111a6d0cbe9790
Tedford <dev@tedfordjohnson.com>
2021-11-30 17:32:08 -0500

    solution - python - raindrops

---
+++ python/raindrops/raindrops.py
@@ -0,0 +1,20 @@
+"""exercise"""
+
+def convert(number):
+    """
+    :param number:
+    :return:
+    """
+    result = ""
+
+    if number % 3 == 0:
+        result += "Pling"
+    if number % 5 == 0:
+        result += "Plang"
+    if number % 7 == 0:
+        result += "Plong"
+
+    if len(result) == 0:
+        result = str(number)
+
+    return result
