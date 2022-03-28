e92d078c6010a58283a24693986c9c98f8224b2b
Tedford <dev@tedfordjohnson.com>
2022-03-28 01:20:33 -0400

    cleanup - rust - lucians lusious lasagna

--- rust/lucians-luscious-lasagna/src/lib.rs
+++ rust/lucians-luscious-lasagna/src/lib.rs
@@ -1,8 +1,6 @@
 // This stub file contains items that aren't used yet; feel free to remove this module attribute
 // to enable stricter warnings.
-#![allow(unused)]
-
-const COOK_TIME : i32= 40;
+const COOK_TIME: i32 = 40;
 const TIME_PER_LAYER: i32 = 2;
 
 pub fn expected_minutes_in_oven() -> i32 {
@@ -18,5 +16,5 @@ pub fn preparation_time_in_minutes(number_of_layers: i32) -> i32 {
 }
 
 pub fn elapsed_time_in_minutes(number_of_layers: i32, actual_minutes_in_oven: i32) -> i32 {
-    preparation_time_in_minutes( number_of_layers) + actual_minutes_in_oven
+    preparation_time_in_minutes(number_of_layers) + actual_minutes_in_oven
 }
