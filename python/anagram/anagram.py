3a06be0ac6f74385cac3f16dc0d774c4e7258bad
Tedford <dev@tedfordjohnson.com>
2022-11-26 00:54:14 -0500

    solution - python - anagram

---
+++ python/anagram/anagram.py
@@ -0,0 +1,2 @@
+def find_anagrams(word, candidates):
+    return [candidate for candidate in candidates if candidate.casefold() != word.casefold() and sorted(candidate.casefold()) == sorted(word.casefold())]
