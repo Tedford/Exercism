bbddc2a64c9ee8847fd7b5421a8682365a372fd7
Tedford Johnson <dev@tedfordjohnson.com>
2023-11-12 11:49:15 -0500

    revised - rust - secret handshake

--- rust/secret-handshake/src/lib.rs
+++ rust/secret-handshake/src/lib.rs
@@ -1,29 +1,17 @@
-// bitmask! {
-//     mask Action: u8 where flags Flags {
-//         Wink = 0b00000001,
-//         DoubleBlink = 0b00000010,
-//         Close = 0b00000100,
-//         Jump = 0b00001000,
-//         Reverse = 0b00010000
-//     }
-// }
-
 pub fn actions(n: u8) -> Vec<&'static str> {
     let mut actions = vec![];
-    if n & 0b00000001 > 0 {
-        actions.push("wink")
-    }
-    if n & 0b00000010 > 0 {
-        actions.push("double blink")
-    }
-    if n & 0b00000100 > 0 {
-        actions.push("close your eyes")
-    }
-    if n & 0b00001000 > 0 {
-        actions.push("jump")
-    }
-    if  n & 0b00010000 > 0 {
-        actions.reverse()
+    let mut mask = 0b00001;
+
+    while mask & 0b11111 > 0 {
+        match mask & n {
+            0b0001 => actions.push("wink"),
+            0b0010 => actions.push("double blink"),
+            0b0100 => actions.push("close your eyes"),
+            0b1000 => actions.push("jump"),
+            0b10000 => actions.reverse(),
+            _ => ()
+        };
+        mask <<= 1;
     }
 
     actions
