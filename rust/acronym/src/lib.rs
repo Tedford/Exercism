3e9c18deaacac7e67bed555c8a23930d4ba44d94
Tedford Johnson <dev@tedfordjohnson.com>
2023-11-12 14:29:51 -0500

    solution - rust - acronym

---
+++ rust/acronym/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn abbreviate(phrase: &str) -> String {
+    phrase
+        .split(|c: char| c.is_whitespace() || c == '-' || c == '_')
+        .flat_map(|token| {
+            token.chars().take(1).chain(
+                token
+                    .chars()
+                    .skip_while(|c| c.is_uppercase())
+                    .filter(|c| c.is_uppercase()),
+            )
+        })
+        .collect::<String>()
+        .to_uppercase()
+}
