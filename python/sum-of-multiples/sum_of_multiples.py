e73fda15632f2b2d1831aee2e34efb0610752a8f
Tedford Johnson <dev@tedfordjohnson.com>
2023-11-21 14:59:02 -0500

    revision - python - sum of multiples

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,8 +1,3 @@
 def sum_of_multiples(limit, multiples):
     multiples = [n for n in multiples if n > 0]
-    factors = []
-    for i in range(1, limit, 1):
-        if len([m for m in multiples if i % m == 0]) > 0:
-            factors.append(i)
-
-    return sum(factors)
+    return sum([i for i in range(1, limit) if len([m for m in multiples if i % m == 0]) > 0])
